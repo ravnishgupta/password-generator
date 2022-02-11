e0eb200a721155bc4e3282da9263a99ddbe14493
Ravnish Gupta <ravnish@gmail.com>
2022-02-10 22:09:35 -0800

    fixed incorrect calling of functions when validation fails

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordProps = {};
 
 // Write password to the #password input
 function writePassword() {
@@ -14,7 +15,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-var passwordProps = {};
+
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
 
@@ -25,25 +26,28 @@ var passwordProps = {};
 
     // WHEN prompted for the length of the password
     // THEN I choose a length of at least 8 characters and no more than 128 characters  
-    passwordProps.length = parseInt(getPasswordLength());
-    console.log("password length: " + passwordProps.length)
+    passwordProps["length"] = parseInt(getPasswordLength());
+    //passwordProps.length = parseInt(getPasswordLength());
+    //console.log("password length: " + passwordProps.length)
 
     // WHEN prompted for character types to include in the password
     // THEN I choose lowercase, uppercase, numeric, and/or special characters
-    password.hasLowerCaseChar = hasLowerCaseChar();
-    console.log("has lower case chars: " + password.hasLowerCaseChar);
+    passwordProps["hasLowerCaseChar"] = hasLowerCaseChar().toUpperCase();
+    // console.log("has lower case chars: " + password.hasLowerCaseChar);
 
-    // THEN I choose uppercase
-    password.hasUpperCaseChar = hasUpperCaseChar();
-    console.log("has upper case chars: " + password.hasUpperCaseChar);
+    // // THEN I choose uppercase
+    passwordProps["hasUpperCaseChar"] = hasUpperCaseChar().toUpperCase();
+    // console.log("has upper case chars: " + password.hasUpperCaseChar);
 
-    // THEN I choose numeric
-    password.hasNumericValues = hasNumericValues();
-    console.log("has numeric values: " + password.hasNumericValues);
+    // // THEN I choose numeric
+    passwordProps["hasNumericValues"] = hasNumericValues().toUpperCase();
+    // console.log("has numeric values: " + password.hasNumericValues);
       
-    // THEN I choose and/or special characters
-    password.hasSpecialChars = hasSpecialChars();
-    console.log("has special characters: " + password.hasSpecialChars);
+    // // THEN I choose and/or special characters
+    passwordProps["hasSpecialChars"] = hasSpecialChars().toUpperCase();
+    // console.log("has special characters: " + password.hasSpecialChars);
+
+    console.log(passwordProps);
   }
 
 
@@ -57,43 +61,58 @@ var passwordProps = {};
 }
 
 function getPasswordLength() {
+  //debugger;
   var input = prompt("Please enter the length of the desired password. The value should be between 8 and 128.");
-  if (input !== null) {
-    if ((parseInt(input) < 8) || (parseInt(input) > 128) || (isNaN(parseInt(input)))) {
-      getPasswordLength();
-    }
-    else return input;
+  if ((input === null) || (input === "")) {
+    return getPasswordLength();
   }
+  else if ((input !== null) && ((parseInt(input) < 8) || (parseInt(input) > 128) || (isNaN(parseInt(input))))) {
+    return getPasswordLength();
+  }
+  else return input;
+  
 }
 
 function hasLowerCaseChar() {
-  var lCaseChar = prompt("Should the password have lowercase characters? Type Yes or No.").toUpperCase();
-  if ((lCaseChar !== "YES") && (lCaseChar !== "NO")) {
-    hasLowerCaseChar();
+  var lCaseChar = prompt("Should the password have lowercase characters? Type Yes or No.");
+  if ((lCaseChar === null) || (lCaseChar === "")) {
+    return hasLowerCaseChar();
+  }
+  else if ((lCaseChar.toUpperCase() !== "YES") && (lCaseChar.toUpperCase() !== "NO")) {
+    return hasLowerCaseChar();
   }
   else return lCaseChar;
 }
 
 function hasUpperCaseChar() {
-  var uCaseChar = prompt("Should the password have uppercase characters? Type Yes or No.").toUpperCase();
-  if ((uCaseChar !== "YES") && (uCaseChar !== "NO")) {
-    hasUpperCaseChar();
+  var uCaseChar = prompt("Should the password have uppercase characters? Type Yes or No.");
+  if ((uCaseChar === null) || (uCaseChar === "")) {
+    return hasUpperCaseChar();
+  }
+  else if ((uCaseChar.toUpperCase() !== "YES") && (uCaseChar.toUpperCase() !== "NO")) {
+    return hasUpperCaseChar();
   }
   else return uCaseChar;
 }
 
 function hasNumericValues() {
-  var numValues = prompt("Should the password have numeric values? Type Yes or No.").toUpperCase();
-  if ((numValues !== "YES") && (numValues !== "NO")) {
-    hasNumericValues();
+  var numValues = prompt("Should the password have numeric values? Type Yes or No.");
+  if ((numValues === null) || (numValues === "")) {
+    return hasNumericValues();
+  }
+  else if ((numValues.toUpperCase() !== "YES") && (numValues.toUpperCase() !== "NO")) {
+    return hasNumericValues();
   }
   else return numValues;
 }
 
 function hasSpecialChars() {
-  var splChar = prompt("Should the password have special characters? Type Yes or No.").toUpperCase();
-  if ((splChar !== "YES") && (splChar !== "NO")) {
-    hasSpecialChars();
+  var splChar = prompt("Should the password have special characters? Type Yes or No.");
+  if ((splChar === null) || (splChar === "")) {
+    return hasSpecialChars();
+  }
+  else if ((splChar.toUpperCase() !== "YES") && (splChar.toUpperCase() !== "NO")) {
+    return hasSpecialChars();
   }
   else return splChar;
 }
